4b320bf778af20bed003f94437178a4d74875091
Tata <tatasamy1998@gmail.com>
2021-03-30 10:12:24 +0200

    dist

--- src/my_main/FXMLDocumentController.java
+++ src/my_main/FXMLDocumentController.java
@@ -29,10 +29,15 @@ public class FXMLDocumentController implements Initializable {
      
      @FXML
     private void static_ip(ActionEvent event) {
-        double ti=Double.parseDouble(time_static.getText());
-       int time=(int)(ti*1000);
-   
-        t.schedule(t1,0,time);
+        
+         try {
+             double ti=Double.parseDouble(time_static.getText());
+            int time=(int)(ti*1000);
+           t.schedule(t1,0,time);
+         } catch (Exception e) {
+             System.out.println("يجب ادخال ارقام فقط");
+         }
+        
     }
     
     private void change_ip(int ip){
@@ -50,26 +55,20 @@ public class FXMLDocumentController implements Initializable {
 	    };
         System.out.println(ip_address);
 	    Process p;
-//		try {
-//			p = Runtime.getRuntime().exec(command);
-//		        new Thread(new SyncPipe(p.getErrorStream(), System.err)).start();
-//	                new Thread(new SyncPipe(p.getInputStream(), System.out)).start();
-//	                PrintWriter stdin = new PrintWriter(p.getOutputStream());
-//	                stdin.println("netsh int ip set address "+adapter_name+" static "+ip_address+" "+subnet_mask+" "+default_gateway);
-//		        stdin.println("netsh int ip set dns "+adapter_name+" static "+dns_1+" primary");
-//	                stdin.println("netsh interface ip add dns "+adapter_name+" "+dns_2+" INDEX=2");
-//                        stdin.close();
-//	                p.waitFor();
-//	    	} catch (Exception e) {
-//	 		e.printStackTrace();
-//		}
+		try {
+			p = Runtime.getRuntime().exec(command);
+		        new Thread(new SyncPipe(p.getErrorStream(), System.err)).start();
+	                new Thread(new SyncPipe(p.getInputStream(), System.out)).start();
+	                PrintWriter stdin = new PrintWriter(p.getOutputStream());
+	                stdin.println("netsh int ip set address "+adapter_name+" static "+ip_address+" "+subnet_mask+" "+default_gateway);
+		        stdin.println("netsh int ip set dns "+adapter_name+" static "+dns_1+" primary");
+	                stdin.println("netsh interface ip add dns "+adapter_name+" "+dns_2+" INDEX=2");
+                        stdin.close();
+	                p.waitFor();
+	    	} catch (Exception e) {
+	 		e.printStackTrace();
+		}
     }
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-         
-        
-    }
-    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         
